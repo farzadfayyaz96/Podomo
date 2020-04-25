1b7d872b8fa3294706848597146d3d0f2bffab9e
farzadfayyaz <farzadfayyaz96@gmail.com>
2020-04-25 23:39:43 +0430

    init project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Podomo.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
